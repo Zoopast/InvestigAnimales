8d911448349deb837083315d6b86b1e112eeecdb
Gabriel San Martin <gsandaur@hotmail.com>
2020-11-08 02:09:18 -0300

    Pagina y BD armada 1.0

--- InvestigAnimalesApp/admin.py
+++ InvestigAnimalesApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Animal
+
+class animalAdmin(admin.ModelAdmin):
+    readonly_fields=('created','update')
+    list_display=["nombre", "especie", "created"]
+    search_fields=["nombre"]
+    list_filter=["especie"]
 
 # Register your models here.
+admin.site.register(Animal, animalAdmin)
--- InvestigAnimalesApp/models.py
+++ InvestigAnimalesApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Animal(models.Model):
+    nombre = models.CharField(max_length=50)
+    especie = models.CharField(max_length=50, default=None)
+    url = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length= 300)
+    imagen = models.ImageField(upload_to='Animal')
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
